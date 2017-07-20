9b9e9b2aa2965f3f23e1ba3d81a97411aeba1804
大蛋散 <439674061@qq.com>
2017-07-21 00:49:49 +0800

    重构代码以去掉重复代码

--- dapeng-remoting/dapeng-remoting-api/src/main/java/com/isuwang/dapeng/remoting/BaseClient.java
+++ dapeng-remoting/dapeng-remoting-api/src/main/java/com/isuwang/dapeng/remoting/BaseClient.java
@@ -1,6 +1,9 @@
 package com.isuwang.dapeng.remoting;
 
+import com.isuwang.dapeng.core.InvocationContext;
+import com.isuwang.dapeng.core.SoaHeader;
 import com.isuwang.dapeng.core.SoaSystemEnvProperties;
+import com.isuwang.dapeng.core.TransactionContext;
 import com.isuwang.dapeng.core.filter.Filter;
 import com.isuwang.dapeng.registry.RegistryAgent;
 import com.isuwang.dapeng.registry.RegistryAgentProxy;
@@ -16,6 +19,9 @@ import org.slf4j.LoggerFactory;
 import javax.xml.bind.JAXB;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by tangliu on 17/7/20.
@@ -24,7 +30,17 @@ public class BaseClient {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(BaseClient.class);
 
-    public static SoaConnectionPool connectionPool;
+    public static final AtomicInteger seqid_ = new AtomicInteger(0);
+
+    protected String serviceName;
+    protected String versionName;
+
+    protected BaseClient(String serviceName, String versionName) {
+        this.serviceName = serviceName;
+        this.versionName = versionName;
+    }
+
+    protected static SoaConnectionPool connectionPool;
 
     static {
 
@@ -83,4 +99,64 @@ public class BaseClient {
 
         return stream;
     }
+
+    protected void initContext(String methodName) {
+        InvocationContext context = InvocationContext.Factory.getCurrentInstance();
+
+        context.setSeqid(seqid_.incrementAndGet());
+
+        SoaHeader soaHeader = context.getHeader() == null ? new SoaHeader() : context.getHeader();
+
+        InvocationContext.Factory.ISoaHeaderProxy headerProxy = InvocationContext.Factory.getSoaHeaderProxy();
+        if (headerProxy != null) {
+            soaHeader.setCallerFrom(headerProxy.callerFrom());
+            soaHeader.setCustomerId(headerProxy.customerId());
+            soaHeader.setCustomerName(headerProxy.customerName());
+            soaHeader.setOperatorId(headerProxy.operatorId());
+            soaHeader.setOperatorName(headerProxy.operatorName());
+            soaHeader.setSessionId(headerProxy.sessionId());
+        }
+
+        //如果在容器内调用其它服务，将原始的调用者信息(customerId/customerName/operatorId/operatorName)传递
+        if (TransactionContext.hasCurrentInstance()) {
+
+            TransactionContext transactionContext = TransactionContext.Factory.getCurrentInstance();
+            SoaHeader oriHeader = transactionContext.getHeader();
+
+            soaHeader.setCustomerId(oriHeader.getCustomerId());
+            soaHeader.setCustomerName(oriHeader.getCustomerName());
+            soaHeader.setOperatorId(oriHeader.getOperatorId());
+            soaHeader.setOperatorName(oriHeader.getOperatorName());
+            soaHeader.setSessionId(oriHeader.getSessionId());
+        }
+
+        soaHeader.setCallerIp(Optional.of(SoaSystemEnvProperties.SOA_CALLER_IP));
+        soaHeader.setServiceName(serviceName);
+        soaHeader.setMethodName(methodName);
+        soaHeader.setVersionName(versionName);
+
+        if (!soaHeader.getCallerFrom().isPresent())
+            soaHeader.setCallerFrom(Optional.of(SoaSystemEnvProperties.SOA_SERVICE_CALLERFROM));
+
+
+        if (!soaHeader.getSessionId().isPresent()) {
+            soaHeader.setSessionId(Optional.of(UUID.randomUUID().toString()));
+        }
+
+        context.setHeader(soaHeader);
+
+        if (context.getCalleeTimeout() <= 0)
+            context.setCalleeTimeout(SoaSystemEnvProperties.SOA_SERVICE_TIMEOUT);
+
+        context.setSoaTransactionProcess(isSoaTransactionalProcess());
+    }
+
+    protected boolean isSoaTransactionalProcess() {
+        return false;
+    }
+
+    protected void destoryContext() {
+        InvocationContext.Factory.removeCurrentInstance();
+    }
+
 }
--- dapeng-remoting/dapeng-remoting-api/src/main/java/com/isuwang/dapeng/remoting/BaseServiceClient.java
+++ dapeng-remoting/dapeng-remoting-api/src/main/java/com/isuwang/dapeng/remoting/BaseServiceClient.java
@@ -22,79 +22,12 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author craneding
  * @date 15/9/24
  */
-public class BaseServiceClient {
+public class BaseServiceClient extends BaseClient {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(BaseServiceClient.class);
 
-    public static final AtomicInteger seqid_ = new AtomicInteger(0);
-
-    protected String serviceName;
-    protected String versionName;
-
     protected BaseServiceClient(String serviceName, String versionName) {
-        this.serviceName = serviceName;
-        this.versionName = versionName;
-    }
-
-    public SoaConnectionPool connectionPool = BaseClient.connectionPool;
-
-    protected void initContext(String methodName) {
-        InvocationContext context = InvocationContext.Factory.getCurrentInstance();
-
-        context.setSeqid(seqid_.incrementAndGet());
-
-        SoaHeader soaHeader = context.getHeader() == null ? new SoaHeader() : context.getHeader();
-
-        InvocationContext.Factory.ISoaHeaderProxy headerProxy = InvocationContext.Factory.getSoaHeaderProxy();
-        if (headerProxy != null) {
-            soaHeader.setCallerFrom(headerProxy.callerFrom());
-            soaHeader.setCustomerId(headerProxy.customerId());
-            soaHeader.setCustomerName(headerProxy.customerName());
-            soaHeader.setOperatorId(headerProxy.operatorId());
-            soaHeader.setOperatorName(headerProxy.operatorName());
-            soaHeader.setSessionId(headerProxy.sessionId());
-        }
-
-        //如果在容器内调用其它服务，将原始的调用者信息(customerId/customerName/operatorId/operatorName)传递
-        if (TransactionContext.hasCurrentInstance()) {
-
-            TransactionContext transactionContext = TransactionContext.Factory.getCurrentInstance();
-            SoaHeader oriHeader = transactionContext.getHeader();
-
-            soaHeader.setCustomerId(oriHeader.getCustomerId());
-            soaHeader.setCustomerName(oriHeader.getCustomerName());
-            soaHeader.setOperatorId(oriHeader.getOperatorId());
-            soaHeader.setOperatorName(oriHeader.getOperatorName());
-            soaHeader.setSessionId(oriHeader.getSessionId());
-        }
-
-        soaHeader.setCallerIp(Optional.of(SoaSystemEnvProperties.SOA_CALLER_IP));
-        soaHeader.setServiceName(serviceName);
-        soaHeader.setMethodName(methodName);
-        soaHeader.setVersionName(versionName);
-
-        if (!soaHeader.getCallerFrom().isPresent())
-            soaHeader.setCallerFrom(Optional.of(SoaSystemEnvProperties.SOA_SERVICE_CALLERFROM));
-
-
-        if (!soaHeader.getSessionId().isPresent()) {
-            soaHeader.setSessionId(Optional.of(UUID.randomUUID().toString()));
-        }
-
-        context.setHeader(soaHeader);
-
-        if (context.getCalleeTimeout() <= 0)
-            context.setCalleeTimeout(SoaSystemEnvProperties.SOA_SERVICE_TIMEOUT);
-
-        context.setSoaTransactionProcess(isSoaTransactionalProcess());
-    }
-
-    protected boolean isSoaTransactionalProcess() {
-        return false;
-    }
-
-    protected void destoryContext() {
-        InvocationContext.Factory.removeCurrentInstance();
+        super(serviceName, versionName);
     }
 
     @SuppressWarnings("unchecked")
