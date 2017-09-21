cce28b6ad3f6b3a68765eff5b43087058a3a89f8
Lihuimin <Lihuimin@iplas.com.cn>
2017-09-21 17:01:54 +0800

    remote-api 服务生成方式改成用serviceLoader

--- dapeng-remoting/dapeng-remoting-api/src/main/java/com/isuwang/dapeng/remoting/BaseClient.java
+++ dapeng-remoting/dapeng-remoting-api/src/main/java/com/isuwang/dapeng/remoting/BaseClient.java
@@ -7,19 +7,12 @@ import com.isuwang.dapeng.core.TransactionContext;
 import com.isuwang.dapeng.core.filter.Filter;
 import com.isuwang.dapeng.registry.RegistryAgent;
 import com.isuwang.dapeng.registry.RegistryAgentProxy;
-import com.isuwang.dapeng.registry.conf.SoaRegistry;
-import com.isuwang.dapeng.remoting.conf.SoaRemoting;
-import com.isuwang.dapeng.remoting.conf.SoaRemotingConnectionPool;
-import com.isuwang.dapeng.remoting.conf.SoaRemotingFilter;
-import com.isuwang.dapeng.remoting.conf.SoaRemotingFilters;
 import com.isuwang.dapeng.remoting.filter.StubFilterChain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.bind.JAXB;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.Optional;
+import java.util.ServiceLoader;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -41,42 +34,30 @@ public abstract class BaseClient {
     protected static SoaConnectionPool connectionPool;
 
     static {
-
-        final ClassLoader classLoader = BaseClient.class.getClassLoader();
-
-        try (InputStream is = getInputStream("dapeng-remoting-conf.xml")) {
-
-            final SoaRemoting soaRemoting = JAXB.unmarshal(is, SoaRemoting.class);
-            final SoaRemotingFilters remotingFilters = soaRemoting.getSoaRemotingFilters();
-
+        try  {
+            ServiceLoader<Filter> filterLoader = ServiceLoader.load(Filter.class);
             // load filter
-            for (SoaRemotingFilter remotingFilter : remotingFilters.getSoaRemotingFilter()) {
-                Class<?> aClass = classLoader.loadClass(remotingFilter.getRef());
-
-                StubFilterChain.addFilter((Filter) aClass.newInstance());
-
-                LOGGER.info("client load filter {} with path {}", remotingFilter.getName(), remotingFilter.getRef());
+            for (Filter filter : filterLoader) {
+                StubFilterChain.addFilter(filter);
             }
-
             // load connection pool
-            final SoaRemotingConnectionPool pool = soaRemoting.getSoaRemotingConnectionPool();
-            final Class<?> aClass = classLoader.loadClass(pool.getRef());
-            BaseClient.connectionPool = (SoaConnectionPool) aClass.newInstance();
-
+            ServiceLoader<SoaConnectionPool>soaConnectionPoolServiceLoader = ServiceLoader.load(SoaConnectionPool.class);
+            for(SoaConnectionPool soaConnectionPool : soaConnectionPoolServiceLoader){
+                BaseClient.connectionPool = soaConnectionPool;
+            }
 
         } catch (Exception e) {
             LOGGER.error("client load filter error", e);
         }
 
         if (!SoaSystemEnvProperties.SOA_REMOTING_MODE.equals("local")) {
-            try (InputStream is = getInputStream("dapeng-registry-conf.xml")) {
-                final SoaRegistry soaRegistry = JAXB.unmarshal(is, SoaRegistry.class);
+            try  {
 
-                Class<?> aClass = classLoader.loadClass(soaRegistry.getRef());
-
-                RegistryAgentProxy.setCurrentInstance(RegistryAgentProxy.Type.Client, (RegistryAgent) aClass.newInstance());
-                RegistryAgentProxy.getCurrentInstance(RegistryAgentProxy.Type.Client).start();
-                LOGGER.info("client load registry {} with path {}", soaRegistry.getName(), soaRegistry.getRef());
+                ServiceLoader<RegistryAgent>registryAgentLoader = ServiceLoader.load(RegistryAgent.class);
+                for (RegistryAgent registryAgent : registryAgentLoader) {
+                    RegistryAgentProxy.setCurrentInstance(RegistryAgentProxy.Type.Client,registryAgent);
+                    RegistryAgentProxy.getCurrentInstance(RegistryAgentProxy.Type.Client).start();
+                }
             } catch (Exception e) {
                 LOGGER.error("client load registry error", e);
             }
@@ -84,18 +65,6 @@ public abstract class BaseClient {
             LOGGER.info("soa remoting mode is {},client not load registry", SoaSystemEnvProperties.SOA_REMOTING_MODE);
         }
 
-    }
-
-    static InputStream getInputStream(String name) throws FileNotFoundException {
-        InputStream stream = BaseClient.class.getClassLoader().getResourceAsStream(name);
-
-        if (stream == null)
-            stream = BaseClient.class.getResourceAsStream(name);
-
-        if (stream == null)
-            throw new FileNotFoundException("not found " + name);
-
-        return stream;
     }
 
     protected void initContext(String methodName) {
