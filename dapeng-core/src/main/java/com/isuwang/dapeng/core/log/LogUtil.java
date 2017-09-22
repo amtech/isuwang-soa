b8a4fce8a460dd0e434d7117a512533f9e9c27a0
jackliang <liangbinbin@iplas.com.cn>
2017-09-22 10:09:42 +0800

    log enhancement

--- dapeng-core/src/main/java/com/isuwang/dapeng/core/log/LogUtil.java
+++ dapeng-core/src/main/java/com/isuwang/dapeng/core/log/LogUtil.java
@@ -1,112 +1,101 @@
 package com.isuwang.dapeng.core.log;
 
 import com.isuwang.dapeng.core.ProcessorKey;
+import com.isuwang.dapeng.core.SoaBaseProcessor;
 import com.isuwang.dapeng.core.SoaHeader;
+import com.isuwang.dapeng.core.helper.SoaHeaderHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 
 /**
  * Created by admin on 2017/9/1.
  */
 public class LogUtil {
 
+    private static SoaHeader getSoaHeader() {
+        return SoaHeaderHelper.getSoaHeader(false);
+    }
+
+    public static void main(String[] args) {
+        SoaHeader soaHeader = getSoaHeader();
+        logInfo(SoaBaseProcessor.class, getSoaHeader(), "request header:{} body:{}", soaHeader.getServiceName(), soaHeader.getVersionName());
+    }
+
     private final static Logger LOGGER = LoggerFactory.getLogger(LogUtil.class);
 
     private static final Map<String, Object> loggerMap = new ConcurrentHashMap<>();
 
-    private static final Map<String,Method> logMethodMap = new ConcurrentHashMap<>();
+    private static final Map<String, Method> logMethodMap = new ConcurrentHashMap<>();
 
-
-
-
-    public static void logInfo(Class<?>logClass,SoaHeader soaHeader,String format,Object ...args){
-        try {
-
-            ProcessorKey key = new ProcessorKey(soaHeader.getServiceName(),soaHeader.getVersionName());
-            ClassLoader appClassLoader = SoaAppClassLoaderCache.getAppClassLoaderMap().get(key);
-            String methodName="info";
-
-            if (appClassLoader!=null) {
-                int classLoaderHex=appClassLoader.hashCode();
-                Object logger=getLogger(appClassLoader,logClass,classLoaderHex);
-                Method infoMethod=getMethod(methodName,logClass,logger,classLoaderHex);
-                infoMethod.invoke(logger,format,args);
-            }else{
-                Logger logger = LoggerFactory.getLogger(logClass);
-                logger.info(format,args);
-            }
-        } catch (Exception e) {
-            //有异常用容器的logger打日志
-            LOGGER.error(e.getMessage());
-            Logger logger = LoggerFactory.getLogger(logClass);
-            logger.info(format,args);
-        }
-
+    public static void logInfo(Class<?> logClass, SoaHeader soaHeader, String formattedMsg, Object... args) {
+        methodInvoke(logClass, soaHeader, "info", logger -> logger.info(formattedMsg, args), new Object[]{formattedMsg, args});
     }
 
+    public static void logError(Class<?> logClass, SoaHeader soaHeader, String errMsg, Throwable exception) {
+        methodInvoke(logClass, soaHeader, "error", logger -> logger.error(errMsg, exception), new Object[]{errMsg, exception});
+    }
 
-
-    public static void logError(Class<?>logClass, SoaHeader soaHeader,String errMsg,Throwable exception){
+    private static void methodInvoke(Class<?> logClass, SoaHeader soaHeader, String methodName, Consumer<Logger> loggerConsumer, Object... args) {
         try {
             ProcessorKey key = new ProcessorKey(soaHeader.getServiceName(), soaHeader.getVersionName());
             ClassLoader appClassLoader = SoaAppClassLoaderCache.getAppClassLoaderMap().get(key);
-            String methodName="error";
 
-            if (appClassLoader!=null) {
-                Object logger=getLogger(appClassLoader,logClass,appClassLoader.hashCode());
-                Method infoMethod=getMethod(methodName,logClass,logger,appClassLoader.hashCode());
-                infoMethod.invoke(logger,errMsg,exception);
-            }else{
-                Logger logger = LoggerFactory.getLogger(logClass);
-                logger.error(errMsg,exception);
+            if (appClassLoader != null) {
+                Object appLogger = getLogger(appClassLoader, logClass, appClassLoader.hashCode());
+                Method infoMethod = getMethod(methodName, logClass, appLogger, appClassLoader.hashCode());
+                infoMethod.invoke(appLogger, args);
+            } else {
+                Logger containerLogger = LoggerFactory.getLogger(logClass);
+                loggerConsumer.accept(containerLogger);
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             //有异常用容器的logger打日志
             LOGGER.error(e.getMessage());
-            Logger logger = LoggerFactory.getLogger(logClass);
-            logger.error(errMsg,exception);
+            Logger containerLogger = LoggerFactory.getLogger(logClass);
+            loggerConsumer.accept(containerLogger);
         }
     }
 
 
     /**
-     *先从logMethodMap查，没有再利用反射获取Method
+     * 先从logMethodMap查，没有再利用反射获取Method
      */
-    public static Method getMethod(String methodName,Class<?>logClass,Object logger,int classLoaderHex)throws Exception{
+    public static Method getMethod(String methodName, Class<?> logClass, Object logger, int classLoaderHex) throws Exception {
         Method method;
-        String logMethodKey= classLoaderHex+"."+logClass.getName()+methodName;
-        if (logMethodMap.containsKey(logMethodKey)){
-            method=logMethodMap.get(logMethodKey);
-        }else{
-            if (methodName.equals("error")){
-                method=logger.getClass().getMethod(methodName,String.class,Throwable.class);
-            }else{
-                method=logger.getClass().getMethod(methodName,String.class,Object[].class);
+        String logMethodKey = classLoaderHex + "." + logClass.getName() + methodName;
+        if (logMethodMap.containsKey(logMethodKey)) {
+            method = logMethodMap.get(logMethodKey);
+        } else {
+            if (methodName.equals("error")) {
+                method = logger.getClass().getMethod(methodName, String.class, Throwable.class);
+            } else {
+                method = logger.getClass().getMethod(methodName, String.class, Object[].class);
             }
 
-            logMethodMap.put(logMethodKey,method);
+            logMethodMap.put(logMethodKey, method);
         }
         return method;
     }
 
     /**
-     *先从loggerMap查，没有再利用反射获取logger
+     * 先从loggerMap查，没有再利用反射获取logger
      */
-    public static Object getLogger(ClassLoader appClassLoader,Class<?>logClass,int classLoaderHex) throws Exception{
+    public static Object getLogger(ClassLoader appClassLoader, Class<?> logClass, int classLoaderHex) throws Exception {
         Object logger;
-        if (loggerMap.containsKey(logClass.getName())){
-            logger=loggerMap.get(logClass.getName());
-        }else{
+        if (loggerMap.containsKey(logClass.getName())) {
+            logger = loggerMap.get(logClass.getName());
+        } else {
             Class<?> logFactoryClass = appClassLoader.loadClass("org.slf4j.LoggerFactory");
             Method getILoggerFactory = logFactoryClass.getMethod("getLogger", Class.class);
             getILoggerFactory.setAccessible(true);
-            logger=getILoggerFactory.invoke(null, logClass);
-            loggerMap.put(classLoaderHex+"."+logClass.getName(),logger);
+            logger = getILoggerFactory.invoke(null, logClass);
+            loggerMap.put(classLoaderHex + "." + logClass.getName(), logger);
         }
         return logger;
     }
