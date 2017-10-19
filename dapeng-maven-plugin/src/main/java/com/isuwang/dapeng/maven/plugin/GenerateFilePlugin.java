fb11ab311a5bbdd45d306f6c7ce1416e355014f5
lihuimin <Lihuimin@iplas.com.cn/iPlas123>
2017-10-19 18:48:49 +0800

    插件路径修改

--- dapeng-maven-plugin/src/main/java/com/isuwang/dapeng/maven/plugin/GenerateFilePlugin.java
+++ dapeng-maven-plugin/src/main/java/com/isuwang/dapeng/maven/plugin/GenerateFilePlugin.java
@@ -7,30 +7,32 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.io.File;
+
 /**
  * Created by jackliang on 2017/10/19.
  */
 @Mojo(name="thriftGenerator")
 public class GenerateFilePlugin extends AbstractMojo {
 
-    @Parameter(property = "thriftGenerator.sourceFilePath", defaultValue = "/tmp/in")
+    @Parameter(property = "thriftGenerator.sourceFilePath", defaultValue = "./src/main/resources/thrift/")
     private String sourceFilePath;
 
-    @Parameter(property = "thriftGenerator.targetFilePath", defaultValue = "/tmp/out")
+    @Parameter(property = "thriftGenerator.targetFilePath", defaultValue = "./src/main/")
     private String targetFilePath;
 
-    @Parameter(property = "thriftGenerator.copyToPath", defaultValue = "./src/main/java/")
-    private String copyToPath;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         System.out.println(" sourceFilePath: " + sourceFilePath);
         System.out.println(" targetFilePath: " + targetFilePath);
-        System.out.println(" copyToPath: " + copyToPath);
 
-        //-gen java -all -in /Users/jackliang/dev/thrift_gen/in -out /Users/jackliang/dev/thrift_gen/out
-        Scrooge.main(new String[]{"-gen","java","-all",
-                "-in","/Users/jackliang/dev/thrift_gen/in",
-                "-out", "/Users/jackliang/dev/thrift_gen/out"});
+        File file = new File("./");
+
+            //-gen java -all -in /Users/jackliang/dev/thrift_gen/in -out /Users/jackliang/dev/thrift_gen/out
+            Scrooge.main(new String[]{"-gen","java","-all",
+                    "-in",sourceFilePath,
+                    "-out",targetFilePath});
+
     }
 }
