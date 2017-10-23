40aefdaf7184a43027670cc4ad7473f7c2e116e4
lihuimin <Lihuimin@iplas.com.cn/iPlas123>
2017-10-23 16:15:46 +0800

    generateFilePlugin修改路径

--- dapeng-maven-plugin/src/main/java/com/isuwang/dapeng/maven/plugin/GenerateFilePlugin.java
+++ dapeng-maven-plugin/src/main/java/com/isuwang/dapeng/maven/plugin/GenerateFilePlugin.java
@@ -6,33 +6,59 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 
 /**
  * Created by jackliang on 2017/10/19.
  */
-@Mojo(name="thriftGenerator")
+@Mojo(name = "thriftGenerator")
 public class GenerateFilePlugin extends AbstractMojo {
 
-    @Parameter(property = "thriftGenerator.sourceFilePath", defaultValue = "./src/main/resources/thrift/")
+    @Parameter(property = "thriftGenerator.sourceFilePath", defaultValue = "src\\main\\resources\\thrift\\")
     private String sourceFilePath;
 
-    @Parameter(property = "thriftGenerator.targetFilePath", defaultValue = "./src/main/")
+    @Parameter(property = "thriftGenerator.targetFilePath", defaultValue = "src\\main\\")
     private String targetFilePath;
+
+    @Parameter(defaultValue = "${project}", readonly = true)
+    protected MavenProject project;
 
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        String projectPath = new File(project.getBuild().getOutputDirectory()).getAbsolutePath().replace("target\\classes", "");
+        String sourceFilePath=projectPath+ "src\\main\\resources\\thrift\\";
+        String targetFilePath=projectPath+"src\\main\\";
+
         System.out.println(" sourceFilePath: " + sourceFilePath);
         System.out.println(" targetFilePath: " + targetFilePath);
 
-        File file = new File("./");
+        Scrooge.main(new String[]{"-gen", "java", "-all",
+                "-in", sourceFilePath,
+                "-out", targetFilePath});
 
-            //-gen java -all -in /Users/jackliang/dev/thrift_gen/in -out /Users/jackliang/dev/thrift_gen/out
-            Scrooge.main(new String[]{"-gen","java","-all",
-                    "-in",sourceFilePath,
-                    "-out",targetFilePath});
+        System.out.println( projectPath+"src\\main\\java\\com\\isuwang\\soa\\common");
+
+        File commonFile = new File(projectPath+"src\\main\\java\\com\\isuwang\\soa\\common");
+        if (commonFile.exists()){
+            deleteDir(commonFile);
+        }
+
 
     }
+    private static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i=0; i<children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
+    }
+
 }
