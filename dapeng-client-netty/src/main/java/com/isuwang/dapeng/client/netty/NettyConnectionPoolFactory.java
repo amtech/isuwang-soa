87c8170f5a2e2f22a08a68937e3ac6beff171c9f
jackliang <liangbinbin@iplas.com.cn>
2017-12-27 00:07:28 +0800

    rename SoaConnctionPoolFactory to SoaConnectionPoolFactory

--- dapeng-client-netty/src/main/java/com/isuwang/dapeng/client/netty/NettyConnectionPoolFactory.java
+++ dapeng-client-netty/src/main/java/com/isuwang/dapeng/client/netty/NettyConnectionPoolFactory.java
@@ -1,12 +1,12 @@
 package com.isuwang.dapeng.client.netty;
 
-import com.isuwang.dapeng.core.SoaConnctionPoolFactory;
+import com.isuwang.dapeng.core.SoaConnectionPoolFactory;
 import com.isuwang.dapeng.core.SoaConnectionPool;
 
 /**
  * Created by lihuimin on 2017/12/24.
  */
-public class NettyConnectionPoolFactory implements SoaConnctionPoolFactory{
+public class NettyConnectionPoolFactory implements SoaConnectionPoolFactory {
 
     private static SoaConnectionPool pool;
 
--- dapeng-core/src/main/java/com/isuwang/dapeng/core/SoaConnectionPoolFactory.java
+++ dapeng-core/src/main/java/com/isuwang/dapeng/core/SoaConnectionPoolFactory.java
@@ -3,7 +3,7 @@ package com.isuwang.dapeng.core;
 /**
  * Created by lihuimin on 2017/12/24.
  */
-public interface SoaConnctionPoolFactory {
+public interface SoaConnectionPoolFactory {
 
     SoaConnectionPool getPool();
 
