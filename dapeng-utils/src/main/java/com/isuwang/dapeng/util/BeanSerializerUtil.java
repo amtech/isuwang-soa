598af8c97e709abec5bbcd787714ca3bc6e9c4b4
lihuimin <Lihuimin@iplas.com.cn/iPlas123>
2018-01-02 18:57:23 +0800

    dapeng-utils

---
+++ dapeng-utils/src/main/java/com/isuwang/dapeng/util/BeanSerializerUtil.java
@@ -0,0 +1,33 @@
+package com.isuwang.dapeng.util;
+
+import com.isuwang.org.apache.thrift.TException;
+import com.isuwang.org.apache.thrift.protocol.TCompactProtocol;
+
+/**
+ * Created by lihuimin on 2017/10/27.
+ */
+public class BeanSerializerUtil {
+
+    public static <T> byte[] serialize(T structBean, com.isuwang.dapeng.core.BeanSerializer<T> structSerializer) throws TException {
+
+        byte [] byteBuf = new byte[8192];
+        final TCommonTransport outputCommonTransport = new TCommonTransport(byteBuf, TCommonTransport.Type.Write);
+
+        TCompactProtocol outputProtocol = new TCompactProtocol(outputCommonTransport);
+        structSerializer.write(structBean, outputProtocol);
+        return outputCommonTransport.getByteBuf();
+    }
+
+    public static <T> T deserialize(byte[] buff, com.isuwang.dapeng.core.BeanSerializer<T> structSerializer) throws TException {
+
+        final TCommonTransport inputCommonTransport = new TCommonTransport(buff, TCommonTransport.Type.Read);
+
+        TCompactProtocol intputProtocol = new TCompactProtocol(inputCommonTransport);
+        T struct = structSerializer.read(intputProtocol);
+        return struct;
+    }
+
+
+
+
+}
